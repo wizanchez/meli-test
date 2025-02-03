b0ab75bd06ec3701ec936c32d248eb6e408022b6
wizMacPro <wizanchez@gmail.com>
2025-02-03 17:04:02 -0500

    feat: change 33

--- src/server/render/template.ts
+++ src/server/render/template.ts
@@ -10,7 +10,7 @@ export const template = (
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="public/styles/main.css">
+    <link rel="stylesheet" type="text/css" href="static/styles/main.css">
     <title>.: Meli</title>
     ${style}
   </head>
